73bc6ff8cf7260cb37c8bed2fcbe9c8e3a5dfd4d
Microgramx <118573098+Microgramx@users.noreply.github.com>
2024-06-05 23:46:31 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export default {
   VITE_SERVER_HOST: import.meta.env.VITE_SERVER_HOST,
   VITE_OAUTH_REDIRECT_HOSTNAME:
-    import.meta.env.VITE_OAUTH_REDIRECT_HOSTNAME ?? 'https://pr-4t2l.vercel.app/',
+    import.meta.env.VITE_OAUTH_REDIRECT_HOSTNAME ?? 'https://pr-roan.vercel.app',
   VITE_FIREBASE_CONFIG:
     import.meta.env.VITE_FIREBASE_CONFIG ??
     '{"apiKey":"AIzaSyA2fkXeFokJ-Ei_jnzDso5AmjbIaMdzuEc","authDomain":"watchparty-273604.firebaseapp.com","databaseURL":"https://watchparty-273604.firebaseio.com","projectId":"watchparty-273604","storageBucket":"watchparty-273604.appspot.com","messagingSenderId":"769614672795","appId":"1:769614672795:web:54bbda86288ab1a034273e"}',
