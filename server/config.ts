ca5fd92dfbacb82a355d1a9b783f4c6cf62ce794
Microgramx <118573098+Microgramx@users.noreply.github.com>
2024-06-05 23:51:06 +0200

    Update config.ts

--- server/config.ts
+++ server/config.ts
@@ -33,7 +33,7 @@ const defaults = {
   SSL_KEY_FILE: '', // Optional, Filename of SSL key (to use https)
   SSL_CRT_FILE: '', // Optional, Filename of SSL cert (to use https)
   PORT: 8080, // Port to use for server
-  HOST: '0.0.0.0', // Host interface to bind server to
+  HOST: 'https://pr-roan.vercel.app/', // Host interface to bind server to
   STATS_KEY: '', // Secret string to validate viewing stats
   BETA_USER_EMAILS: '', // Comma-delimited list of user emails to include in the beta
   CUSTOM_SETTINGS_HOSTNAME: 'https://pr-roan.vercel.app/', // Hostname to send different config settings to client
